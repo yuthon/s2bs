c7d00cfcf0f6a3e36662963933b1838c15b03cb1
yuthon <yh88gr4@gmail.com>
2022-02-23 11:30:09 +0900

    TooltipをAppコンポーネントのマウント時に初期化できるよう編集

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
 import './App.css';
 import Main from './Main';
+import { Tooltip } from 'bootstrap';
 
 function App() {
+  // すべてのツールチップを初期化 中身の関数はReact公式から引用
+  React.useEffect(() => {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+      var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl);
+      });
+  },[])
   return (
     <div className="App">
       <Main />
