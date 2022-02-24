906e231c1a8b38d02786dcaa681f1ac621b3bcad
yuthon <yh88gr4@gmail.com>
2022-02-24 12:29:42 +0900

    スロット部分のツールチップのホバー領域を丸くするためにsvgを導入

--- src/SlotComponents/MainSlot0.js
+++ src/SlotComponents/MainSlot0.js
@@ -1,13 +1,21 @@
 
 const MainSlot0 = (props) => {
     return(
-      <div className="btn" data-bs-toggle="tooltip" data-bs-placement="top" title="クリックでギアパワーを変更">
+      <div className="">
+          
         <div
           className="ability-icon main-slot slot"
           data-bs-toggle="modal"
           data-bs-target="#mainSlotModal0"
         >
-          <img src={props.imagePath} alt=""/>
+          <div className="slot-tooltip">  
+            <svg viewBox="0 0 200 200" width="58" height="58">
+              <a href="#" className="d-inline-block" data-bs-toggle="tooltip" data-bs-placement="top" title="クリックでギアパワーを変更">
+                <circle cx="100" cy="100" r="100" opacity="0"/>
+              </a>
+            </svg>
+          </div>
+          <img className="slot-img" src={props.imagePath} alt=""/>
         </div>
       </div>
     );
