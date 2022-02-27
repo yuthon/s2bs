8d9ac362c7448df6730d21151eb2c7998943eb6b
yuthon <yh88gr4@gmail.com>
2022-02-27 14:02:36 +0900

    アビリティ関連のStateの更新がMainコンポーネントを再レンダーしてほしくなかったので関連するコンポーネントのStateをAbilityComponents.jsに移動

--- src/AbilityComponents.js
+++ src/AbilityComponents.js
@@ -1,9 +1,9 @@
-// アビリティ関連のコンポーネント
-import AbilityComponents from './AbilityComponents';
+import { useState } from 'react';
 //ブキ
 import Weapon from './WeaponComponents/Weapon';
 import WeaponModal from './WeaponComponents/WeaponModal';
-
+// アビリティ効果コンポーネント
+import AbilityEffect from './AbilityEffectComponents/AbilityEffect';
 //ギア選択のモーダル
 import HeadGearModal from './HeadComponents/HeadGearModal';
 import ClothesGearModal from './ClothesComponents/ClothesGearModal';
@@ -22,23 +22,12 @@ import SubSlotModal5 from './SlotComponents/SubSlotModal5';
 import SubSlotModal6 from './SlotComponents/SubSlotModal6';
 import SubSlotModal7 from './SlotComponents/SubSlotModal7';
 import SubSlotModal8 from './SlotComponents/SubSlotModal8';
-//キャラクターの詳細設定モーダル
-import CharacterTypeModal from './CharacterSettingComponents/CharacterTypeModal';
-import SkinColorModal from './CharacterSettingComponents/SkinColorModal';
-import IFMLHairStyleModal from './CharacterSettingComponents/IFMLHairStyleModal';
-import IMLHairStyleModal from './CharacterSettingComponents/IMLHairStyleModal';
-import OFMLHairStyleModal from './CharacterSettingComponents/OFMLHairStyleModal';
-import OMLHairStyleModal from './CharacterSettingComponents/OMLHairStyleModal';
-import MLBottomsModal from './CharacterSettingComponents/MLBottomsModal';
-import FMLBottomsModal from './CharacterSettingComponents/FMLBottomsModal';
-import AbilityEffect from './AbilityEffectComponents/AbilityEffect';
-
+// アタマ
 import Head from './HeadComponents/Head';
+// フク
 import Clothes from './ClothesComponents/Clothes';
 // クツ
 import Shoes from './ShoesComponents/Shoes';
-
-import ThreeComponent from './ThreeComponent';
 //初期表示用の画像
 import WakabaIkaT from './images/gear/Clt_FST001.png';
 import Canvas_White from './images/gear/Shs_FST000.png';
@@ -46,18 +35,7 @@ import Headband_White from './images/gear/Hed_FST000.png';
 import empty from './images/ability/empty.png';
 import Jr_0 from './images/weapon/Splattershot Jr.png';
 
-import { useState } from 'react';
-// 3dモデル
-import Model from './ThreeComponents/Model';
-import Orbit from './ThreeComponents/Orbit';
-import { Suspense } from 'react';
-import {Canvas, extend } from '@react-three/fiber';
-
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-extend({ OrbitControls });
-
-
-const Main = () => {
+const AbilityComponents = () => {
   //ブキの名前とその他の情報を管理
   const [weaponStatus, passWeaponStatus] = useState({name:"わかばシューター", sub:"SplatBomb", special:"Armor", sp:180, weight:"light", path:Jr_0});
   //選択されたアタマの画像のパスを管理
@@ -100,81 +78,111 @@ const Main = () => {
   const [subAbilityName6, passSubAbilityName6] = useState(null);
   const [subAbilityName7, passSubAbilityName7] = useState(null);
   const [subAbilityName8, passSubAbilityName8] = useState(null);
-  //キャラ設定モーダルを管理
-  const [characterType, passCharacterType] = useState("IML");
-  const [skinColor, passSkinColor] = useState("#ffd1cc");
-  const [hairStyle, passHairStyle] = useState("Hair_0");
-  const [eyeColor, passEyeColor] = useState("#1617ff");
-  const [isMLBottomsModalOpen, controlMLBottomsModal] = useState(false);
-  const [isFMLBottomsModalOpen, controlFMLBottomsModal] = useState(false);
-  const [bottoms, passBottoms] = useState("Btm_0");
-  const [teamColor, passTeamColor] = useState("ffb23d");
   
-  return(
-    <div className="container">
-      <CharacterTypeModal
-        passCharacterType={passCharacterType}
-        characterType={characterType} />
-      <SkinColorModal
-        passSkinColor={passSkinColor}
-        characterType={characterType} />
-      <IFMLHairStyleModal
-        passHairStyle={passHairStyle} />
-      <IMLHairStyleModal
-        passHairStyle={passHairStyle} />
-      <OFMLHairStyleModal
-        passHairStyle={passHairStyle} />
-      <OMLHairStyleModal
-        passHairStyle={passHairStyle} />
-      <MLBottomsModal
-        passBottoms={passBottoms} />
-      <FMLBottomsModal
-        passBottoms={passBottoms} />
-      <div className="row">  
-        <AbilityComponents />
-        <div className="col-5 order-2" id="three-section">
-          <div style={{ height: '100vh', width: '100%' }}>
-            <div id="character-btn">
-              <button type="button" className="btn btn-dark font-type1" data-bs-toggle="modal" data-bs-target="#characterTypeModal">
-                キャラクターの設定
-              </button>
-            </div>
-            <Canvas
-              style={{ background: '#f0f0e0' }}
-              camera={{ position: [0, 11, 20] }}
-            >
-              <ambientLight
-                color={0xffffff}
-                intensity={0.8} />
-              <directionalLight
-                color={0xffffff}
-                intensity={0.6} />
-              <pointLight
-                position={[0, 11, 20]}
-                intensity={0.7} />
-              <pointLight
-                position={[0, 11, -20]}
-                intensity={0.7} />
-              <Orbit />
-              <axesHelper args={[5]} />
-              <Suspense fallback={null}>
-                <Model
-                  headGear={chosenHeadGear}
-                  clothesGear={chosenClothesGear}
-                  shoesGear={chosenShoesGear}
-                  CT={characterType}
-                  skinColor={skinColor}
-                  eyeColor={eyeColor}
-                  hairStyle={hairStyle}
-                  bottoms={bottoms}
-                  teamColor={teamColor} />
-              </Suspense>
-            </Canvas>
-          </div>
-        </div>
+  return (
+    <><div className="gear-components col-2 order-1">
+      <div>
+        <Head
+          //ギアモーダルからStateを経由して受け取った画像のパスをpropsで渡す
+          passHeadGearImagePath={headImagePath}
+          //SubSlotModal1から受け取ったアビリティの画像のパスを渡す
+          passSubAbilityImagePath0={subAbilityImagePath0}
+          //2,3についても同様
+          passSubAbilityImagePath1={subAbilityImagePath1}
+          passSubAbilityImagePath2={subAbilityImagePath2}
+          //メインスロットも同様
+          passMainAbilityImagePath={mainAbilityImagePath0} />
+        <MainSlotModal0
+          passImagePath={passMainAbilityImagePath0}
+          passAbilityName={passMainAbilityName0} />
+        <HeadGearModal
+          //モーダルで選択されたギアの名前を受け取る
+          passChosenGear={passChosenHeadGear}
+          //ギア画像のパスを受け取る
+          passImagePath={passHeadImagePath} />
+        <SubSlotModal0
+          passAbilityName={passSubAbilityName0}
+          passImagePath={passSubAbilityImagePath0} />
+        <SubSlotModal1
+          passAbilityName={passSubAbilityName1}
+          passImagePath={passSubAbilityImagePath1} />
+        <SubSlotModal2
+          passAbilityName={passSubAbilityName2}
+          passImagePath={passSubAbilityImagePath2} />
+      </div>
+      <div>
+        <Clothes
+          passClothesGearImagePath={clothesImagePath}
+          passSubAbilityImagePath3={subAbilityImagePath3}
+          passSubAbilityImagePath4={subAbilityImagePath4}
+          passSubAbilityImagePath5={subAbilityImagePath5}
+          passMainAbilityImagePath={mainAbilityImagePath1} />
+        <ClothesGearModal
+          passChosenGear={passChosenClothesGear}
+          passImagePath={passClothesImagePath} />
+        <MainSlotModal1
+          passImagePath={passMainAbilityImagePath1}
+          passAbilityName={passMainAbilityName1} />
+        <SubSlotModal3
+          passAbilityName={passSubAbilityName3}
+          passImagePath={passSubAbilityImagePath3} />
+        <SubSlotModal4
+          passAbilityName={passSubAbilityName4}
+          passImagePath={passSubAbilityImagePath4} />
+        <SubSlotModal5
+          passAbilityName={passSubAbilityName5}
+          passImagePath={passSubAbilityImagePath5} />
+      </div>
+      <div>
+        <Shoes
+          passShoesGearImagePath={shoesImagePath}
+          passSubAbilityImagePath6={subAbilityImagePath6}
+          passSubAbilityImagePath7={subAbilityImagePath7}
+          passSubAbilityImagePath8={subAbilityImagePath8}
+          passMainAbilityImagePath={mainAbilityImagePath2} />
+        <ShoesGearModal
+          passChosenGear={passChosenShoesGear}
+          passImagePath={passShoesImagePath} />
+        <MainSlotModal2
+          passImagePath={passMainAbilityImagePath2}
+          passAbilityName={passMainAbilityName2} />
+        <SubSlotModal6
+          passAbilityName={passSubAbilityName6}
+          passImagePath={passSubAbilityImagePath6} />
+        <SubSlotModal7
+          passAbilityName={passSubAbilityName7}
+          passImagePath={passSubAbilityImagePath7} />
+        <SubSlotModal8
+          passAbilityName={passSubAbilityName8}
+          passImagePath={passSubAbilityImagePath8} />
       </div>
     </div>
+    <div className="font-type2 col-5 order-3" id="ae-section">
+      <div>
+        <WeaponModal
+          weaponStatus={passWeaponStatus} 
+        />
+        <Weapon
+          weaponStatus={weaponStatus} 
+        />
+      </div>
+      <AbilityEffect
+        weaponStatus={weaponStatus}
+        passMainAbilityName0={mainAbilityName0}
+        passMainAbilityName1={mainAbilityName1}
+        passMainAbilityName2={mainAbilityName2}
+        passSubAbilityName0={subAbilityName0}
+        passSubAbilityName1={subAbilityName1}
+        passSubAbilityName2={subAbilityName2}
+        passSubAbilityName3={subAbilityName3}
+        passSubAbilityName4={subAbilityName4}
+        passSubAbilityName5={subAbilityName5}
+        passSubAbilityName6={subAbilityName6}
+        passSubAbilityName7={subAbilityName7}
+        passSubAbilityName8={subAbilityName8} 
+      />
+    </div></>
   );
-};
+}
 
-export default Main;
+export default AbilityComponents;
--- src/WeaponComponents/Weapon.js
+++ src/WeaponComponents/Weapon.js
@@ -1,3 +1,32 @@
+// サブとスペシャル
+import CurlingBomb from '../images/subSP/Wsb_Bomb_Curling.png';
+import FizzyBomb from '../images/subSP/Wsb_Bomb_Fizzy.png';
+import BurstBomb from '../images/subSP/Wsb_Bomb_Burst.png';
+import AutoBomb from '../images/subSP/Wsb_Bomb_Auto.png';
+import SplatBomb from '../images/subSP/Wsb_Bomb_Splat.png';
+import SuctionBomb from '../images/subSP/Wsb_Bomb_Suction.png';
+import Torpedo from '../images/subSP/Wsb_Torpedo.png';
+import SquidBeakon from '../images/subSP/Wsb_SquidBeakon.png';
+import PointSensor from '../images/subSP/Wsb_PointSensor.png';
+import ToxicMist from '../images/subSP/Wsb_ToxicMist.png';
+import SplashWall from '../images/subSP/Wsb_SplashWall.png';
+import Sprinkler from '../images/subSP/Wsb_Sprinkler.png';
+import InkMine from '../images/subSP/Wsb_InkMine.png';
+import Baller from '../images/subSP/Wsp_Baller.png';
+import Inkjet from '../images/subSP/Wsp_Inkjet.png';
+import CurlingRush from '../images/subSP/Wsp_CurlingBombLauncher.png';
+import BurstRush from '../images/subSP/Wsp_BurstBombLauncher.png';
+import AutoRush from '../images/subSP/Wsp_AutoBombLauncher.png';
+import SplatRush from '../images/subSP/Wsp_SplatBombLauncher.png';
+import SuctionRush from '../images/subSP/Wsp_SuctionBombLauncher.png';
+import InkStorm from '../images/subSP/Wsp_InkStorm.png';
+import InkArmor from '../images/subSP/Wsp_InkArmor.png';
+import BooyahBomb from '../images/subSP/Wsp_BooyahBomb.png';
+import BubbleBlower from '../images/subSP/Wsp_BubbleBlower.png';
+import SplashDown from '../images/subSP/Wsp_SplashDown.png';
+import TentaMissile from '../images/subSP/Wsp_TentaMissile.png';
+import UltraStamp from '../images/subSP/Wsp_UltraStamp.png';
+import Stingray from '../images/subSP/Wsp_Stingray.png';
 
 const Weapon = (props) => {
   return (
