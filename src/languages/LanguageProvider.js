8d0447f1a6feefaac5dce69877a389d6ac4ebb17
yuthon <yh88gr4@gmail.com>
2022-04-11 20:27:39 +0900

    LanguageProvider.jsを追加

---
+++ src/languages/LanguageProvider.js
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+import { languageOptions, dictionaryList } from "./languages";
+
+// デフォルトの言語を元に言語のコンテキストを作る
+export const LanguageContext = createContext({
+  userLanguage: "en",
+  dictionary: dictionaryList.en
+});
+
+// 
+export function LanguageProvider({ children }) {
+  const defaultLanguage = window.localStorage.getItem("rcml-lang");
+  const [userLanguage, setUserLanguage] = useState(defaultLanguage || "en");
+
+  const provider = {
+    userLanguage,
+    dictionary: dictionaryList[userLanguage],
+    userLanguageChange: selected => {
+      const newLanguage = languageOptions[selected] ? selected : "en";
+      setUserLanguage(newLanguage);
+      window.localStorage.setItem("rcml-lang", newLanguage)
+    }
+  };
+  return (
+    <LanguageContext.Provider value={provider}>
+      {children}
+    </LanguageContext.Provider>
+  )
+}
+
---
+++ src/languages/languages.js
@@ -0,0 +1,6 @@
+import ja from './js.json';
+import en from './en.json';
+
+export const dictionaryList = { en, ja };
+
+export const languageOptions = { en: "English", ja: "Japanese" };
